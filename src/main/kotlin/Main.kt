ca9000f7d722655b817f1f9b01369204e76acc6d
Ethan Choi <gom@avivland.com>
2017-10-17 21:24:44 +0900

    update_all

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,45 @@
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import data.Repository
 import net.ProjectExtractor
+import java.io.File
 import java.nio.file.Paths
 
 fun main(args : Array<String>)
 {
-    ProjectExtractor(Paths.get("projects.json")).extract()
+//    var hashMap = HashMap<String, Repository>()
+//    var str = File("projects.json").readText()
+//    var list : List<SearchResponse> = Gson().fromJson(str, Array<SearchResponse>::class.java).toList()
+//
+//    list.forEach {
+//        it.items.forEach {
+//            item ->
+//            if(!hashMap.containsKey(item.full_name))
+//                hashMap.put(item.full_name, item);
+//        }
+//    }
+//
+//    File("projects_map.json").printWriter().use { out
+//        ->
+//        out.print(GsonBuilder().setPrettyPrinting().create().toJson(hashMap))
+//    }
+//    "ss".clone()
+//    File("dd").printWriter()
+//    ProjectExtractor(Paths.get("projects_map.json")).extract(31)
+
+    var map : HashMap<String, Repository> = Gson().fromJson(File("projects_map.json").readText(), object : TypeToken<HashMap<String, Repository>>() {}.type)
+
+    map.values.forEach({
+        it.full_name.clone()
+    })
+
+}
+
+inline fun String.clone()
+{
+    println(this)
 }
 
 data class SearchResponse(var items:List<Repository>, var total_count:Int)
@@ -15,3 +48,7 @@ data class SearchResponse(var items:List<Repository>, var total_count:Int)
         override fun deserialize(content: String) = Gson().fromJson(content, SearchResponse::class.java)
     }
 }
+
+
+
+
--- src/main/kotlin/net/ProjectExtractor.kt
+++ src/main/kotlin/net/ProjectExtractor.kt
@@ -1,31 +1,45 @@
 package net
 
 import SearchResponse
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import data.Repository
 import java.io.PrintWriter
 import java.nio.file.Path
 
-class ProjectExtractor(val savePath: Path, val limit:Int = 5000)
+class ProjectExtractor(val savePath: Path)
 {
-    fun extract()
+    fun extract(page : Int = 0)
     {
-        var results = ArrayList<SearchResponse>()
+        var results = HashMap<String, Repository>()
         var totalCount = 0
         val requestBuilder = RequestBuilder()
+
+        if(savePath.toFile().exists())
+            results = Gson().fromJson(savePath.toFile().readText(), object : TypeToken<HashMap<String, Repository>>() {}.type)
+
+        requestBuilder.page = page;
 
         while(true)
         {
             val (request, response, result) = requestBuilder.build().responseObject(SearchResponse.Deserializer());
-            val(search, err) = result
+            val (search, err) = result
 
             search?.let {
-                results.add(search);
+
+                search.items.forEach({
+                    if(!results.containsKey(it.full_name)) {
+                        results.put(it.full_name, it);
+                        println("added ${it.full_name}")
+                    }
+                })
+
                 totalCount += search.items.size;
-                println("Added current : $totalCount / $limit page : ${requestBuilder.page}")
             }
 
-            if (err != null || totalCount > limit)
-                break
+            if(err != null)
+                break;
             else
                 requestBuilder.page++;
         }
